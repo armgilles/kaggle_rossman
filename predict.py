2ea9673220154bc3638456e81714437ae1499142
Armand Gilles <arm.gilles@gmail.com>
2015-11-18 18:26:22 +0100

    xgboost with new features. Not best model

--- predict.py
+++ predict.py
@@ -1,31 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 18 11:48:04 2015
-
-@author: babou
-"""
 import pandas as pd
 import numpy as np
 from sklearn.cross_validation import train_test_split
 import xgboost as xgb
-import operator
-import matplotlib
-matplotlib.use("Agg") #Needed to save figures
-import matplotlib.pyplot as plt
-
-def create_feature_map(features):
-    outfile = open('xgb.fmap', 'w')
-    for i, feat in enumerate(features):
-        outfile.write('{0}\t{1}\tq\n'.format(i, feat))
-    outfile.close()
-
-def rmspe(y, yhat):
-    return np.sqrt(np.mean((yhat/y-1) ** 2))
-
-def rmspe_xg(yhat, y):
-    y = np.expm1(y.get_label())
-    yhat = np.expm1(yhat)
-    return "rmspe", rmspe(y,yhat)
 
 # Gather some features
 def build_features(features, data):
@@ -33,26 +9,46 @@ def build_features(features, data):
     data.fillna(0, inplace=True)
     data.loc[data.Open.isnull(), 'Open'] = 1
     # Use some properties directly
-    features.extend(['Store', 'CompetitionDistance', 'CompetitionOpenSinceMonth',
-                     'CompetitionOpenSinceYear', 'Promo', 'Promo2', 'Promo2SinceWeek',
-                     'Promo2SinceYear'])
+    features.extend(['Store', 'CompetitionDistance', 'Promo', 'Promo2', 'SchoolHoliday'])
 
-    # add some more with a bit of preprocessing
-    features.append('SchoolHoliday')
-    data['SchoolHoliday'] = data['SchoolHoliday'].astype(float)
-
+    # Label encode some features
     features.extend(['StoreType', 'Assortment', 'StateHoliday'])
     mappings = {'0':0, 'a':1, 'b':2, 'c':3, 'd':4}
     data.StoreType.replace(mappings, inplace=True)
     data.Assortment.replace(mappings, inplace=True)
     data.StateHoliday.replace(mappings, inplace=True)
 
-    features.extend(['DayOfWeek', 'month', 'day', 'year'])
-    data['year'] = data.Date.dt.year
-    data['month'] = data.Date.dt.month
-    data['day'] = data.Date.dt.day
+    features.extend(['DayOfWeek', 'Month', 'Day', 'Year', 'WeekOfYear'])
+    data['Year'] = data.Date.dt.year
+    data['Month'] = data.Date.dt.month
+    data['Day'] = data.Date.dt.day
     data['DayOfWeek'] = data.Date.dt.dayofweek
+    data['WeekOfYear'] = data.Date.dt.weekofyear
 
+    # CompetionOpen en PromoOpen from https://www.kaggle.com/ananya77041/rossmann-store-sales/randomforestpython/code
+    # Calculate time competition open time in months
+    features.append('CompetitionOpen')
+    data['CompetitionOpen'] = 12 * (data.Year - data.CompetitionOpenSinceYear) + \
+        (data.Month - data.CompetitionOpenSinceMonth)
+    # Promo open time in months
+    features.append('PromoOpen')
+    data['PromoOpen'] = 12 * (data.Year - data.Promo2SinceYear) + \
+        (data.WeekOfYear - data.Promo2SinceWeek) / 4.0
+    data['PromoOpen'] = data.PromoOpen.apply(lambda x: x if x > 0 else 0)
+
+    # Indicate that sales on that day are in promo interval
+    features.append('IsPromoMonth')
+    month2str = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', \
+             7:'Jul', 8:'Aug', 9:'Sept', 10:'Okt', 11:'Nov', 12:'Dec'}
+    data['monthStr'] = data.Month.map(month2str)
+    data.loc[data.PromoInterval == 0, 'PromoInterval'] = ''
+    data['IsPromoMonth'] = 0
+    for interval in data.PromoInterval.unique():
+        if interval != '':
+            for month in interval.split(','):
+                data.loc[(data.monthStr == month) & (data.PromoInterval == interval), 'IsPromoMonth'] = 1
+
+    return data
 
 ## Start of main script
 
@@ -61,19 +57,18 @@ types = {'CompetitionOpenSinceYear': np.dtype(int),
          'CompetitionOpenSinceMonth': np.dtype(int),
          'StateHoliday': np.dtype(str),
          'Promo2SinceWeek': np.dtype(int),
-         'SchoolHoliday': np.dtype(float),
+         'SchoolHoliday': np.dtype(int),
          'PromoInterval': np.dtype(str)}
 train = pd.read_csv("input/train.csv", parse_dates=[2], dtype=types)
 test = pd.read_csv("input/test.csv", parse_dates=[3], dtype=types)
 store = pd.read_csv("input/store.csv")
 
 print("Assume store open, if not provided")
-train.fillna(1, inplace=True)
 test.fillna(1, inplace=True)
 
 print("Consider only open stores for training. Closed stores wont count into the score.")
 train = train[train["Open"] != 0]
-print("Use only Sales bigger then zero. Simplifies calculation of rmspe")
+print("Use only Sales bigger then zero")
 train = train[train["Sales"] > 0]
 
 print("Join with store")
@@ -83,22 +78,34 @@ test = pd.merge(test, store, on='Store')
 features = []
 
 print("augment features")
-build_features(features, train)
-build_features([], test)
+train = build_features(features, train)
+test = build_features([], test)
 print(features)
 
 print('training data processed')
 
+def rmspe(y, yhat):
+    return np.sqrt(np.mean(((y - yhat)/y) ** 2))
+
+def rmspe_xg(yhat, y):
+    y = np.expm1(y.get_label())
+    yhat = np.expm1(yhat)
+    return "rmspe", rmspe(y, yhat)
+
+print("Train xgboost model")
+
 params = {"objective": "reg:linear",
           "booster" : "gbtree",
-          "eta": 0.09,
+          "eta": 0.1,
           "max_depth": 10,
-          "subsample": 0.9,
-          "colsample_bytree": 0.7,
+          "subsample": 0.85,
+          "colsample_bytree": 0.4,
+          "min_child_weight": 6,
           "silent": 1,
+          "thread": 1,
           "seed": 1301
           }
-num_boost_round = 1700
+num_boost_round = 1400
 
 print("Train a XGBoost model")
 X_train, X_valid = train_test_split(train, test_size=0.012, random_state=10)
@@ -108,8 +115,8 @@ dtrain = xgb.DMatrix(X_train[features], y_train)
 dvalid = xgb.DMatrix(X_valid[features], y_valid)
 
 watchlist = [(dtrain, 'train'), (dvalid, 'eval')]
-gbm = xgb.train(params, dtrain, num_boost_round, evals=watchlist, \
-  early_stopping_rounds=100, feval=rmspe_xg, verbose_eval=True)
+gbm = xgb.train(params, dtrain, num_boost_round, evals=watchlist, early_stopping_rounds=200, \
+  feval=rmspe_xg, verbose_eval=True)
 
 print("Validating")
 yhat = gbm.predict(xgb.DMatrix(X_valid[features]))
@@ -121,20 +128,4 @@ dtest = xgb.DMatrix(test[features])
 test_probs = gbm.predict(dtest)
 # Make Submission
 result = pd.DataFrame({"Id": test["Id"], 'Sales': np.expm1(test_probs)})
-result.to_csv("xgboost_10_submission.csv", index=False)
-
-# XGB feature importances
-# Based on https://www.kaggle.com/mmueller/liberty-mutual-group-property-inspection-prediction/xgb-feature-importance-python/code
-
-create_feature_map(features)
-importance = gbm.get_fscore(fmap='xgb.fmap')
-importance = sorted(importance.items(), key=operator.itemgetter(1))
-
-df = pd.DataFrame(importance, columns=['feature', 'fscore'])
-df['fscore'] = df['fscore'] / df['fscore'].sum()
-
-featp = df.plot(kind='barh', x='feature', y='fscore', legend=False, figsize=(6, 10))
-plt.title('XGBoost Feature Importance')
-plt.xlabel('relative importance')
-fig_featp = featp.get_figure()
-fig_featp.savefig('feature_importance_xgb.png', bbox_inches='tight', pad_inches=1)
+result.to_csv("submission.csv", index=False)
